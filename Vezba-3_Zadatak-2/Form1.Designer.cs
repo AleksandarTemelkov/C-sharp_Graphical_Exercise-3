560b6761a63599b20fad5443c1bf5b3fb75686bd
AleksandarTemelkov <temelkovaleksa@gmail.com>
2023-03-21 14:00:23 +0100

    2023/03/21 | 14:00

--- Vezba-3_Zadatak-2/Form1.Designer.cs
+++ Vezba-3_Zadatak-2/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_ReadLine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 200);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button_ReadLine
+            // 
+            this.button_ReadLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.button_ReadLine.Location = new System.Drawing.Point(150, 275);
+            this.button_ReadLine.Name = "button_ReadLine";
+            this.button_ReadLine.Size = new System.Drawing.Size(200, 50);
+            this.button_ReadLine.TabIndex = 1;
+            this.button_ReadLine.Text = "Čitaj Red";
+            this.button_ReadLine.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button_ReadLine);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_ReadLine;
     }
 }
 
